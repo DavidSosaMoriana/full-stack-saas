b8e09a5588dc5283939449c81b93dadefe6bd796
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-07-01 17:47:52 +0100

    Porject Modal

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Workers from '../components/Workers'
 import Projects from '../components/Projects'
 import AddWorkerModal from '../components/AddWorkerModal'
+import AddProjectModal from '../components/AddProjectModal'
 
 export default function Home() {
   return (
     <>
     <div className='d-flex gap-3 mb-4'>
       <AddWorkerModal />
+      <AddProjectModal />
     </div>
      <Projects />
      <hr />
